9dcd44819f6a82debcd4cc14138b6e50cf8173e8
Yuri Ziebell <hallo@yuriziebell.de>
2017-04-09 16:18:38 +0200

    clean css

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-import './Contact.compact.css'
+import './Contact.scss'
 
 class Contact extends Component {
   render () {
--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import './NewExpense.compact.css'
+import './NewExpense.scss'
 
 class NewExpense extends Component {
   render () {
