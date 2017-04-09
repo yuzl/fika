fba04492f2febfdef5ea98d6b10296c19192da6a
tilmx <tilman@Tilmans-MacBook-Pro.local>
2017-04-10 00:20:03 +0200

    slider.html: swiper experiments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,3 @@
-$(document).ready(function () {
-    $('.user').slick({
-        centerMode: true,
-        slidesToShow: 5,
-        arrows: false,
-        infinite: false,
-        adaptiveHeight: true,
-        swipeToSlide: true,
-        focusOnSelect: true,
-        //centerPadding: '1%',
-        variableWidth: true,
-        touchThreshold: 100,
-    });
-});
-
-
-
 $(".keyboard-numpad div").click(function(event){
     $('#amount').val($('#amount').val() + $(this).text());
 });
