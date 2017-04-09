a118dbc3fbb64e9518990b84daa8c7061c512f4f
tilmx <tilman@Tilmans-MacBook-Pro.local>
2017-04-09 11:42:39 +0200

    Amount (keyboard opens on click)

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ $(document).ready(function () {
 $(".keyboard-numpad div").click(function(event){
     $('#amount').val($('#amount').val() + $(this).text());
 });
+
+$(".keyboard-save").click(function(event) {
+    $(".keyboard").removeClass("open");
+})
+
+$(".transaction").click(function(event) {
+    $(".keyboard").addClass("open");
+})
