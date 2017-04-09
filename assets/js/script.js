9014547312f26099d16d29166d8ac0ce301d20cc
tilmx <tilman@Tilmans-MacBook-Pro.local>
2017-04-09 23:42:51 +0200

    Gallery adaptions, swiper files downloaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
-    
+    $('.user').slick({
+        centerMode: true,
+        slidesToShow: 5,
+        arrows: false,
+        infinite: false,
+        adaptiveHeight: true,
+        swipeToSlide: true,
+        focusOnSelect: true,
+        //centerPadding: '1%',
+        variableWidth: true,
+        touchThreshold: 100,
+    });
 });
+
+
 
 $(".keyboard-numpad div").click(function(event){
     $('#amount').val($('#amount').val() + $(this).text());
