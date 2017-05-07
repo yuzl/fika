5a4d61664d3d5479ed520135ebd993c078b3fd41
tilmx <tilman@Tilmans-MacBook-Pro.local>
2017-05-07 20:49:17 +0200

    Funny things

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,11 @@ var active = 1;
 var items = bubbles.getElementsByClassName("gallery-item");
 
 bubbles.onscroll = function(event) {
+    //items.classList = "gallery-item";
     items[active].classList = "gallery-item active";
     items[active-1].classList = "gallery-item previous";
     items[active+1].classList = "gallery-item next";
+    
     
     if(this.scrollLeft > (items[active].offsetLeft - items[0].offsetWidth/2) ) {
         active++;
@@ -14,6 +16,10 @@ bubbles.onscroll = function(event) {
     if(this.scrollLeft < (items[active-1].offsetLeft - items[0].offsetWidth/2) ) {
         active--;
     }
+    
+    var zoom = (this.scrollLeft - items[active-1].offsetLeft) / 100;
+    document.body.style.setProperty('--zoom', zoom);
+    console.log(zoom);
 }
 
 
