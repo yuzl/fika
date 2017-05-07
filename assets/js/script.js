d861b8e29db99c7f2d49a03f9dd17481bf04b575
tilmx <tilman@Tilmans-MacBook-Pro.local>
2017-05-07 18:09:40 +0200

    JS versuche

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
-$(document).ready(function () {
-    
+var myElement = document.getElementById('myElement');
+
+/*
+// create a simple instance
+// by default, it only adds horizontal recognizers
+var mc = new Hammer(myElement);
+
+// let the pan gesture support all directions.
+// this will block the vertical scrolling on a touch-device while on the element
+
+// listen to events...
+mc.on("panleft panright", function(ev) {
+    console.log(ev.type +" gesture detected.");
 });
+
+*/
+
+myElement.onscroll = function(event) {
+    var scrolled = this.scrollLeft;
+    //document.body.style.setProperty('--bla', this.scrollLeft);
+    console.log(scrolled);
+    
+    if(scrolled == 125) {
+        this.classList.add('active');
+    }
+    else {
+        this.classList.remove('active');
+    }
+}
+
+//object.addEventListener("scroll", myScript);
