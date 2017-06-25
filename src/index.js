10a0a941100687e1a8515476509361b04854f59e
Frederik Reiß <frederik.reiss@sinnerschrader.com>
2017-06-25 15:42:34 +0200

    Removed sass dependencies

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { Provider } from 'mobx-react'
 import stores from './stores'
 import App from './container'
 
+import './index.css'
+import './normalize.css'
+
 ReactDOM.render(
   <Provider user={stores.user} contacts={stores.contacts} expenses={stores.expenses}>
     <App />
