dcba2b5e67873bf691f270821481643910c00f2f
Frederik Reiß <frederik.reiss@stud.fh-luebeck.de>
2017-06-25 15:10:38 +0200

    removed index.scss
    TODO: Remove old SASS components

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import { Provider } from 'mobx-react'
 import stores from './stores'
 import App from './container'
 
-import './index.scss'
-
 ReactDOM.render(
   <Provider user={stores.user} contacts={stores.contacts} expenses={stores.expenses}>
     <App />
